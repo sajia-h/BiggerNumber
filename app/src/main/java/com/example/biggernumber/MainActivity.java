9c8a2e58e1d7e70e678e6ea9692b73fc53eef5a9
Sajia <1716573@brunel.ac.uk>
2019-10-23 11:03:21 +0100

    start of project

---
+++ app/src/main/java/com/example/biggernumber/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.biggernumber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
